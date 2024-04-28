a8c3bc4bdddd2f8ff16f092a6797d8632804eb11
useusername1 <reknvqvjoq123@gmail.com>
2024-04-28 23:40:34 +0900

    chore:image 파일 구글 드라이브로 주소 변경

--- Client/src/components/Login_Sign/Overlay.tsx
+++ Client/src/components/Login_Sign/Overlay.tsx
@@ -6,13 +6,16 @@ import { ReactComponent as Logo } from "../../data/Logo.svg";
 interface OverlayProps {
   direction: "left" | "right";
 }
+const imageUrl =
+  "https://lh3.googleusercontent.com/d/1G4EUcTwL5eH9r-lGmrV3bohOtomnTBW6";
+
 const Overlay = ({ direction }: OverlayProps) => {
   const overlays = useRecoilValue<boolean>(setOverlay);
   const navigate = useNavigate();
 
   return (
     <o.StyledOverlay overlay={overlays} isLeft={direction === "left"}>
-      <img src={process.env.PUBLIC_URL + "loginImage.jpg"}></img>
+      <img src={imageUrl}></img>
       <o.LogoContainer>
         <Logo
           style={{ width: "80px", height: "50px" }}
