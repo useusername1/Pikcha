ec8dd0b365583604a1273e543476ef13293a5d86
useusername1 <reknvqvjoq123@gmail.com>
2024-08-06 23:10:50 +0900

    feat:사용자 전용 페이지를 위한 Protected Route 구현

--- Client/src/recoil/auth/index.ts
+++ Client/src/recoil/auth/index.ts
@@ -1,4 +1,11 @@
 import { UserDataAtomFamily } from "./atoms";
 import { withUserDefaultFamily } from "./withLocalstorage";
+import { withTokenExpirationTime, withTokenRenewalRequired } from "./withToken";
 import { UserStateKeys } from "./types";
-export { UserDataAtomFamily, withUserDefaultFamily, UserStateKeys };
+export {
+  UserDataAtomFamily,
+  withUserDefaultFamily,
+  withTokenExpirationTime,
+  withTokenRenewalRequired,
+  UserStateKeys,
+};
